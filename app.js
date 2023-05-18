c36e7e862c539b7b37e2f055224f3e9653643b1d
Doug McPeek <dmcpeek@outlook.com>
2023-05-18 15:36:00 -0600

    Check in

--- app.js
+++ app.js
@@ -4,7 +4,33 @@ console.log(
 );
 
 // Exercise 1
+const firstName = "Doug";
+let lastName = "";
+var age;
 
 // Exercise 2
+lastName = "McPeek";
+age = 57;
+
 
 // Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+
+// Exercise 4
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+
+// Exercise 5
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 466;
+let hasRead = true;
+
+// Just moved console.log(inStock) - down the page.
